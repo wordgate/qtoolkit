2c99cc6ce7619ddc9dc77feac68ce110b114fa0b
bestesther <info@allnationconnect.com>
2026-03-19 21:37:47 +0800

    refactor(fastgpt,chatwoot): remove env var fallback, single source of truth via viper config only

--- fastgpt/fastgpt.go
+++ fastgpt/fastgpt.go
@@ -16,7 +16,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"os"
 	"strings"
 	"sync"
 	"time"
@@ -38,18 +37,10 @@ var (
 )
 
 func loadConfigFromViper() *Config {
-	cfg := &Config{
+	return &Config{
 		APIKey:  viper.GetString("fastgpt.api_key"),
 		BaseURL: viper.GetString("fastgpt.base_url"),
 	}
-	// Environment variable fallback
-	if env := os.Getenv("FASTGPT_API_KEY"); env != "" {
-		cfg.APIKey = env
-	}
-	if env := os.Getenv("FASTGPT_BASE_URL"); env != "" {
-		cfg.BaseURL = env
-	}
-	return cfg
 }
 
 func initialize() {
